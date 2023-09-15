5a4e7b62caa8cd315c1134a490b16471cabb208d
hememh <makhmutov_tagir@bk.ru>
2023-09-15 13:18:00 +0300

    yy

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors(
     {credentials:true,
-    origin:'http://localhost:3000'}
+    origin:'https://mongo-site3.vercel.app'}
 ))
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
