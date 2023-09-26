0c13ffdc32bc384f63932bdf2b78d4e462254bf7
hememh <makhmutov_tagir@bk.ru>
2023-09-26 09:30:53 +0300

    new2

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors(
     {credentials:true,
-    origin:'http://localhost:3000'}
+    origin:'https://photoprocat-client.vercel.app'}
 ))
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
